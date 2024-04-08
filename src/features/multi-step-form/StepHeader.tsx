00fd2046441e9e078c89a2e491a9708e903f8cb9
Mirror83 <glenochieng045@gmail.com>
2024-04-08 22:51:00 +0300

    Increased the font-size of the title h1 element

--- src/features/multi-step-form/StepHeader.tsx
+++ src/features/multi-step-form/StepHeader.tsx
@@ -6,7 +6,7 @@ interface StepHeaderProps {
 function StepHeader({ title, description }: StepHeaderProps) {
   return (
     <div className="my-8">
-      <h1 className="text-2xl font-bold mb-2">{title}</h1>
+      <h1 className="text-4xl font-bold mb-2">{title}</h1>
       <p className="text-gray-500">{description}</p>
     </div>
   )
