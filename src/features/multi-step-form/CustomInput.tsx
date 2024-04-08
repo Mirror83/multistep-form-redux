4111c2012bb338733e3f57c7bd9f55628937e82d
Mirror83 <glenochieng045@gmail.com>
2024-04-08 22:49:43 +0300

    Added more fields to allow CustomInput to work with react-hook-form's register API

--- src/features/multi-step-form/CustomInput.tsx
+++ src/features/multi-step-form/CustomInput.tsx
@@ -1,20 +1,35 @@
+import { FieldValues, RegisterOptions, UseFormRegister } from "react-hook-form"
+
 interface CustomInputProps {
   name: string
   label: string
   type: string
   id: string
+  defaultValue: string
   placeholder?: string
+  register: UseFormRegister<FieldValues>
+  registerOptions?: RegisterOptions<FieldValues, string>
 }
-function CustomInput({ name, label, type, id, placeholder }: CustomInputProps) {
+function CustomInput({
+  name,
+  label,
+  type,
+  id,
+  placeholder,
+  defaultValue,
+  register,
+  registerOptions,
+}: CustomInputProps) {
   return (
     <div className="flex flex-col flex-wrap gap-1 my-4">
       <label htmlFor={id}>{label}</label>
       <input
         className="outline outline-1 p-2 rounded"
-        name={name}
         type={type}
         id={id}
         placeholder={placeholder}
+        defaultValue={defaultValue}
+        {...register(name, { ...registerOptions })}
       />
     </div>
   )
