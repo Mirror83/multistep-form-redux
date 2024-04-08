eecd0e5a181201c4f7491a42e9f3f1b171b544e7
Mirror83 <glenochieng045@gmail.com>
2024-04-08 22:56:01 +0300

    Styled the element layout to obey the the desktop layout of the frontend-mentor design

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import Sidebar from "./features/multi-step-form/Sidebar"
 function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <div className="md:w-9/12 md:rounded md:grid md:grid-cols-2 p-4 bg-white">
+      <div className="md:w-9/12 md:h-5/6 md:rounded md:grid md:grid-cols-[1fr_3fr] p-4 bg-white">
         <Sidebar />
-        <Outlet />
+        <section className="flex flex-col flex-wrap items-center">
+          <div className="w-4/6">
+            <Outlet />
+          </div>
+        </section>
       </div>
     </div>
   )
