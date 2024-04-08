c5a547523ec35b8c56e1543bbac26bd16b584cdb
Mirror83 <glenochieng045@gmail.com>
2024-04-08 22:52:39 +0300

    Styled the sidebar a tiny bit.

--- src/features/multi-step-form/Sidebar.tsx
+++ src/features/multi-step-form/Sidebar.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Sidebar() {
   return (
-    <div className="flex flex-col flex-wrap gap-4">
+    <div className="flex flex-col flex-wrap gap-4 text-white bg-blue-700 rounded p-8">
       <SidebarComponent step={1} stepDescription="Your info" />
       <SidebarComponent step={2} stepDescription="Select Plan" />
       <SidebarComponent step={3} stepDescription="Add-ons" />
@@ -18,9 +18,14 @@ interface SidebarComponentProps {
 
 function SidebarComponent({ step, stepDescription }: SidebarComponentProps) {
   return (
-    <div>
-      <div>Step {step}</div>
-      <div>{stepDescription}</div>
+    <div className="flex gap-4 items-center">
+      <div className="flex items-center justify-center w-8 h-8 rounded-full outline outline-white outline-1">
+        {step}
+      </div>
+      <div className="uppercase">
+        <p className="font-light text-sm text-gray-200">Step {step}</p>
+        <p className="font-bold">{stepDescription}</p>
+      </div>
     </div>
   )
 }
