c4d89af87846404e332deaf9443204438ad00893
Mirror83 <glenochieng045@gmail.com>
2024-04-09 18:42:36 +0300

    Made some minor tweaks to the styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Sidebar from "./features/multi-step-form/Sidebar"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <div className="md:w-9/12 md:h-5/6 md:rounded md:grid md:grid-cols-[1fr_3fr] p-4 bg-white">
+    <div className="w-screen h-screen flex items-center justify-center bg-magnolia">
+      <div className="md:w-9/12 md:h-5/6 md:rounded-lg md:grid md:grid-cols-[3fr_6fr] md:shadow-lg p-4 bg-white">
         <Sidebar />
         <section className="flex flex-col flex-wrap items-center">
           <div className="w-4/6">
