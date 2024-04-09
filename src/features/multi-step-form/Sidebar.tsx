5633eab96a795490455cdc36e03aea386699c384
Mirror83 <glenochieng045@gmail.com>
2024-04-09 18:45:11 +0300

    Unconventionally added the background image and added conditional styling based on current step.

--- src/features/multi-step-form/Sidebar.tsx
+++ src/features/multi-step-form/Sidebar.tsx
@@ -1,12 +1,44 @@
-import React from "react"
+import { cn } from "@/lib/utils"
+import { useLocation } from "react-router-dom"
+
+function useStepNumber() {
+  const location = useLocation()
+  const locationParts = location.pathname.split("/")
+  const currentPage = locationParts[locationParts.length - 1]
+
+  if (currentPage === "") return 1
+
+  const stepNumber = parseInt(currentPage.split("-")[1])
+
+  if (stepNumber === 5) return 4
+
+  return stepNumber
+}
+
+const steps = ["Your info", "Select Plan", "Add-ons", "Summary"]
 
 function Sidebar() {
+  const stepNumber = useStepNumber()
+
   return (
-    <div className="flex flex-col flex-wrap gap-4 text-white bg-blue-700 rounded p-8">
-      <SidebarComponent step={1} stepDescription="Your info" />
-      <SidebarComponent step={2} stepDescription="Select Plan" />
-      <SidebarComponent step={3} stepDescription="Add-ons" />
-      <SidebarComponent step={4} stepDescription="Summary" />
+    <div className="flex flex-col justify-between text-white rounded-lg relative">
+      <div className="flex flex-wrap flex-col gap-4 p-8 z-10">
+        {steps.map((step, index) => (
+          <SidebarComponent
+            key={index}
+            step={index + 1}
+            stepDescription={step}
+            currentStep={stepNumber}
+          />
+        ))}
+      </div>
+
+      <div className="absolute z-0 h-full w-full">
+        <img
+          src="/assets/images/bg-sidebar-desktop.svg"
+          className="object-cover h-full w-full rounded-lg"
+        />
+      </div>
     </div>
   )
 }
@@ -14,12 +46,22 @@ function Sidebar() {
 interface SidebarComponentProps {
   step: number
   stepDescription: string
+  currentStep: number
 }
 
-function SidebarComponent({ step, stepDescription }: SidebarComponentProps) {
+function SidebarComponent({
+  step,
+  stepDescription,
+  currentStep,
+}: SidebarComponentProps) {
   return (
     <div className="flex gap-4 items-center">
-      <div className="flex items-center justify-center w-8 h-8 rounded-full outline outline-white outline-1">
+      <div
+        className={cn(
+          "flex items-center justify-center w-8 h-8 rounded-full outline outline-white outline-1",
+          currentStep === step && "bg-pastel-blue text-black",
+        )}
+      >
         {step}
       </div>
       <div className="uppercase">
