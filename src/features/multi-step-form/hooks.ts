3311281841f7a05f3bf12bbc42d8b3fd0b73e183
Mirror83 <glenochieng045@gmail.com>
2024-04-10 05:23:59 +0300

    Added useStepNumber hook

---
+++ src/features/multi-step-form/hooks.ts
@@ -0,0 +1,15 @@
+import { useLocation } from "react-router-dom"
+
+export function useStepNumber() {
+  const location = useLocation()
+  const locationParts = location.pathname.split("/")
+  const currentPage = locationParts[locationParts.length - 1]
+
+  if (currentPage === "") return 1
+
+  const stepNumber = parseInt(currentPage.split("-")[1])
+
+  if (stepNumber === 5) return 4
+
+  return stepNumber
+}
