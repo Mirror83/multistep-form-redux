e28b3ece6cfa015542b27a0b6a62b7649dc52011
Mirror83 <glenochieng045@gmail.com>
2024-04-10 05:26:32 +0300

    Modified styles and added the NavigationButtons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "./features/multi-step-form/Sidebar"
+import NavigationButtons from "./features/multi-step-form/NavigationButtons"
 
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-magnolia">
-      <div className="md:w-9/12 md:h-5/6 md:rounded-lg md:grid md:grid-cols-[3fr_6fr] md:shadow-lg p-4 bg-white">
+    <div className="min-w-screen min-h-screen flex items-center justify-center bg-magnolia">
+      <div className="md:w-8/12 md:min-w-[700px] md:h-5/6 md:min-h-[550px] md:rounded-lg md:grid md:grid-cols-[4fr_8fr] md:shadow-lg p-4 bg-white">
         <Sidebar />
-        <section className="flex flex-col flex-wrap items-center">
-          <div className="w-4/6">
+        <section className="flex flex-wrap justify-center">
+          <div className="w-5/6">
             <Outlet />
           </div>
+          <NavigationButtons className="flex justify-between self-end w-5/6 py-4" />
         </section>
       </div>
     </div>
