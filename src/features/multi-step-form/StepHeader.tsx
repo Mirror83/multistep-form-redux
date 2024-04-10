02ff49f09bc6f6b50ba1117cc78442838444bb69
Mirror83 <glenochieng045@gmail.com>
2024-04-10 05:28:07 +0300

    Changed text colours to match the design

--- src/features/multi-step-form/StepHeader.tsx
+++ src/features/multi-step-form/StepHeader.tsx
@@ -5,9 +5,9 @@ interface StepHeaderProps {
 
 function StepHeader({ title, description }: StepHeaderProps) {
   return (
-    <div className="my-8">
+    <div className="my-4 text-marine-blue">
       <h1 className="text-4xl font-bold mb-2">{title}</h1>
-      <p className="text-gray-500">{description}</p>
+      <p className="text-cool-gray">{description}</p>
     </div>
   )
 }
