8bb18dd31564de4f286da97b098762db2d0d6acd
Mirror83 <glenochieng045@gmail.com>
2024-04-12 16:22:47 +0300

    Added the conntent for Step5

--- src/features/multi-step-form/Step5.tsx
+++ src/features/multi-step-form/Step5.tsx
@@ -1,10 +1,20 @@
-import React from "react"
-import { Link } from "react-router-dom"
-
 function Step5() {
   return (
-    <div>
-      Done <Link to={"/"}>Back Home</Link>
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <img
+        src="/assets/images/icon-thank-you.svg"
+        role="presentation"
+        alt="checkmark icon"
+        className="w-16 h-16"
+      />
+      <h1 className="text-4xl font-bold text-marine-blue mt-8 mb-4">
+        Thank You!
+      </h1>
+      <p className="text-cool-gray text-center">
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at{" "}
+        <span className="font-bold">support@loremgaming.com.</span>
+      </p>
     </div>
   )
 }
