d3649c3342a6e7c66b13bbd8fc45ef32fb0a103c
Mirror83 <glenochieng045@gmail.com>
2024-04-22 10:28:46 +0300

    Removed useMediaQuery hook.

--- src/features/multi-step-form/hooks.ts
+++ src/features/multi-step-form/hooks.ts
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
 export function useStepNumber() {
@@ -11,22 +10,4 @@ export function useStepNumber() {
   const stepNumber = parseInt(currentPage.split("-")[1])
 
   return stepNumber
-}
-
-export function useMediaQuery(query: string) {
-  const mediaQueryList = window.matchMedia(query)
-
-  const [matches, setMatches] = useState(mediaQueryList.matches)
-
-  useEffect(() => {
-    const handleMediaQuery = (e: MediaQueryListEvent) => {
-      console.log(e.matches)
-      setMatches(e.matches)
-    }
-    mediaQueryList.addEventListener("change", handleMediaQuery)
-
-    return mediaQueryList.removeEventListener("change", handleMediaQuery)
-  }, [])
-
-  return matches
 }
