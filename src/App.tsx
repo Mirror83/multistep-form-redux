0ec6377ae748f5c91d1c5237efcddc6afa305451
Mirror83 <glenochieng045@gmail.com>
2024-04-26 10:16:53 +0300

    Constrained with of the sidebar to the intrinsic width of the background image, and made a few minor layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
 
   return (
     <div className="min-w-screen min-h-screen md:flex md:items-center md:justify-center">
-      <div className="relative md:w-9/12 md:min-w-[750px] md:h-5/6 md:min-h-[550px] md:rounded-lg md:grid md:grid-cols-[4fr_8fr] md:gap-8 md:shadow-lg md:p-4 md:bg-white">
+      <div className="relative md:w-9/12 md:min-w-[750px] md:max-w-[1240px] md:rounded-lg md:grid md:grid-cols-[274px_8fr] md:gap-8 md:shadow-lg md:p-4 md:bg-white">
         <Sidebar />
-        <div className="flex justify-center md:flex md:flex-wrap md:justify-center md:bg-inherit">
+        <div className="flex justify-center md:flex md:flex-wrap md:justify-center">
           {/** The calculation of min-h (calc(100vh - 6rem)) removes the 6rem that was added by top-24 to the height.*/}
-          <div className="absolute top-24 flex flex-col justify-between items-center min-h-[calc(100vh-6rem)] min-w-screen max-w-[640px] md:w-[95%] md:min-h-max md:relative md:top-0 md:mx-0 md:rounded-none md:shadow-none">
+          <div className="absolute top-24 w-full flex flex-col justify-between items-center min-h-[calc(100vh-6rem)] min-w-screen max-w-[640px] md:w-[90%] md:min-h-max md:relative md:top-0 md:mx-0 md:rounded-none md:shadow-none">
             <main
               className={cn(
                 "bg-white w-[95%] rounded-lg shadow-md p-4 md:rounded-none md:shadow-none mb-8 md:mb-0",
